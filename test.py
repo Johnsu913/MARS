7cfb766686941ef197d35db4eb9a5a48592b2e2f
Johnsu913 <67899743+Johnsu913@users.noreply.github.com>
2020-07-10 19:36:55 +1200

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-#import matplotlib.pyplot as plt
 df = pd.read_csv('Base.csv')
 
 Time = df[' TimeSinceLoad(s)']
